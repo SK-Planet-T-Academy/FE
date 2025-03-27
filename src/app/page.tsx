7c855685c0b85ec80b6e7f9fddf6b7064cfcc078
ohofront <youngho7482@gmail.com>
2025-03-28 03:57:28 +0900

    feat: :sparkles: 로그인 상태(localStorage)에 따라 조건부 리다이렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
-// src/app/page.tsx
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  redirect("/register");
+  const router = useRouter();
+
+  useEffect(() => {
+    const isLogin = localStorage.getItem("isLogin") === "true";
+    console.log("isLogin", isLogin);
+
+    if (isLogin) {
+      router.replace("/board");
+    } else {
+      router.replace("/login");
+    }
+  }, [router]);
+
+  return null; // 리다이렉트용이라 렌더링할 내용 없음
 }
